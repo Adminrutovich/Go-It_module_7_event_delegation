4512b96781755e6361691fb4d6f756b57a690d40
Adminrutovich <112871323+Adminrutovich@users.noreply.github.com>
2023-07-17 15:41:07 +0300

    Change 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarcup = createImageColection(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarcup);
+
+
+function createImageColection(galleryItems) {
+    return  galleryItems
+    .map(({preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="large-image.jpg">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+        `
+    })
+    .join("");
+
+
+}
+
+// console.log(createImageColection(galleryItems));
+
+const galleryEventListener = document.querySelector("ul.gallery");
+// console.log(galleryEventListener)
+galleryEventListener.addEventListener("click", getModalPicture);
+galleryEventListener.addEventListener('keydown', closeByEsc); 
+
+let modalWindow;
+
+function getModalPicture (event) {
+    event.preventDefault();
+    // console.log(event.target.dataset.source);
+    const imgUrl = event.target.dataset.source
+    modalWindow = basicLightbox.create(`
+    <img width="1400" height="auto" src="${imgUrl}">
+    `);
+    modalWindow.show()
+
+}
+function closeByEsc(evt) {
+    // console.log(evt.keyCode);
+    if(evt.keyCode === 27){
+        modalWindow.close()
+    } 
+}
+
